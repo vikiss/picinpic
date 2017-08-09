97b9c6c7dd5c8b93ee32815196e0d023ae636235
vikiss <vikis@skdn.com>
2017-08-09 13:26:54 +0000

    added image field

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,34 @@ Route::get('/projects', function() {
 	return view('projectsview', ['projects' => $projects]);
 });
 
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+
+use Illuminate\Http\Request;
+
+Route::post('/submit', function(Request $request) {
+    $validator = Validator::make($request->all(), [
+        'title' => 'required|max:255',
+        'url' => 'active_url|required|max:255',
+        'description' => 'required|max:255',
+	'image' => 'max:255',
+    ]);
+    if ($validator->fails()) {
+        return back()
+            ->withInput()
+            ->withErrors($validator);
+    }
+    $link = new \App\Project;
+    $link->title = $request->title;
+    $link->url = $request->url;
+    $link->description = $request->description;
+	$link->image = $request->image;
+    $link->save();
+    return redirect('/');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
